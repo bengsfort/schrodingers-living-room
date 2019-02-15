8dee7f544a59419ee33ebf668ffefdf2abddf5f6
Reetu Kontio <reetu.kontio@gmail.com>
2019-02-15 23:27:41 +0200

    added base for interactable and gamestate

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour
+{
+    private enum State { GAME, WON, LOST }
+
+    private State mGameState;
+    private float mGameTime;
+
+    public float mStartingTime;
+
+    private void GameOver()
+    {
+        if (mGameState == State.WON) Debug.Log("Won");
+        else Debug.Log("Lost");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mGameTime = mStartingTime;
+        mGameState = State.GAME;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mGameTime > 0f && mGameState == State.GAME)
+        {
+            mGameTime -= Time.deltaTime;
+        }
+        else if (mGameState == State.GAME)
+        {
+            mGameState = State.WON;
+            GameOver();
+        }
+        Debug.Log(mGameTime);
+    }
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private float mRatio;
+
+    public Sprite mLivingSprite;
+    public Sprite mDeadSprite;
+
+    public void Interact()
+    {
+        ChangeRatio(0.1f);
+    }
+
+    public void ChangeRatio(float change)
+    {
+        mRatio += change;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // TODO: highlight or something?
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
