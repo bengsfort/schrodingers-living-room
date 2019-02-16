4e2034d12d7a505c86e0edf11554a24636996189
Simon N <sbn@jim1000sprog.dk>
2019-02-16 11:18:06 +0200

    Cat ggrfx controller

---
+++ Assets/Scripts/CatGraphicsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatGraphicsController : MonoBehaviour
+{
+
+	public enum CatDirections {
+		Left,
+		Right
+	}
+
+	public CatDirections direction = CatDirections.Left;
+
+	public enum CatActions {
+		Standing,
+		Walking,
+		Scratching
+	}
+
+	public CatActions action = CatActions.Standing;
+
+	private Material cachedMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Renderer r = GetComponent<Renderer>();
+		if (r != null) {
+			cachedMaterial = r.material;
+			r.material.shader = Shader.Find("Sch/Characters/Cat");
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
