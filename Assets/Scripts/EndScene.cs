58e93835de99576af3ac013a3f389b0b28ea43c4
alintulu <adelinalintu@gmail.com>
2019-02-17 16:03:51 +0200

    Credits

---
+++ Assets/Scripts/EndScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScene : MonoBehaviour
+{
+	public void LoadMain() {
+		SceneManager.LoadScene("IntroScreen", LoadSceneMode.Single);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.anyKey) {
+			LoadMain ();
+		}
+    }
+}
